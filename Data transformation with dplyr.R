d085c71ec835dbc06daaeaf0e091ce7764660ffb
sanyalh <sanyalh@yahoo.com>
2017-11-12 01:02:23 -0500

    Add files via upload

--- Data transformation with dplyr.R
+++ Data transformation with dplyr.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(nycflight13)
 
+# 1a
 (a<-filter(flights,arr_delay>=120))
 
+# 1b
 (a<-filter(flights,dest=='IAH'|dest=='HOU'))
 View(a)
 
@@ -18,4 +20,8 @@ group_by(points,txt1) %>% summarize(val1=mean(val1,na.rm='true'))
 
 group_by(flights,carrier) %>% summarize(val1=mean(dep_delay,na.rm='true'))
 
+# 1c
 filter(flights,carrier=='DL'|carrier=='UA'|carrier=='AA')
+
+# 1d
+View(filter(flights,month=='7'|month=='8'|month=='9'))
