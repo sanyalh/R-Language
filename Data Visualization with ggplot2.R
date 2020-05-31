9c5d5a73695a58783ad08852ab2eba9c9cdb84ce
sanyalh <sanyalh@yahoo.com>
2020-05-31 12:07:12 -0400

    Add files via upload

--- Data Visualization with ggplot2.R
+++ Data Visualization with ggplot2.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
+# 3.2.4 
+
+
+# 3.3.1 
 #1
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
@@ -16,3 +20,17 @@ ggplot(data = mpg) +
 #4
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, color=displ))
+
+
+#5
+?geom_point
+
+# For shapes that have a border (like 21), you can colour the inside and
+# outside separately. Use the stroke aesthetic to modify the width of the
+# border
+ggplot(mtcars, aes(wt, mpg)) +
+  geom_point(shape = 21, colour = "yellow", fill = "blue", size = 5, stroke = 5)
+
+#6
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color=displ < 5))
